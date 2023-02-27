c12cc458d9fbfe1b681f46842c834bd27bbfdca5
hamid313-coder <hamid.hussainy0@gmail.com>
2023-02-27 15:53:43 +0430

    rebuiled animation from zero

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -14,46 +14,48 @@ class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    print("ehlloooo");
     final screenSize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: const Color(0xffFEE5CA),
       body: ScrollTransformView(
         children: [
           ScrollTransformItem(
-            builder: (offset) {
-              final screenOffPercentage = min(offset / screenSize.height, 1);
+            builder: (scrollOffset) {
+              final screenOffsetPercent =
+                  min(scrollOffset / screenSize.height, 1);
 
               return Image.network(
-                backgroundImageUrl,
                 height: screenSize.height -
-                    (screenSize.height * 0.2 * screenOffPercentage),
+                    (screenSize.height * 0.2 * screenOffsetPercent),
                 width: screenSize.width -
-                    (screenSize.width * 0.5 * screenOffPercentage),
+                    (screenSize.width * 0.5 * screenOffsetPercent),
+                backgroundImageUrl,
                 fit: BoxFit.cover,
               );
             },
-            offsetBuilder: (offset) {
-              final screenOffPercentage = min(offset / screenSize.height, 1);
-              final heightShrinkageAmount =
-                  screenSize.height * 0.2 * screenOffPercentage;
-              final double onScreenOffset = offset + heightShrinkageAmount / 2;
-              final isStartMoving = offset >= screenSize.height;
+            offsetBuilder: (scrollOffset) {
+              final screenOffsetPercent =
+                  min(scrollOffset / screenSize.height, 1);
+              final shrinkageAmount =
+                  screenSize.height * 0.2 * screenOffsetPercent;
+
+              final onScreenOffset = scrollOffset + shrinkageAmount / 2;
+
+              final isStartMoving = scrollOffset >= screenSize.height * 0.85;
 
               return Offset(
                   0,
                   isStartMoving
-                      ? onScreenOffset - (offset - screenSize.height)
+                      ? onScreenOffset -
+                          (scrollOffset - screenSize.height * 0.85)
                       : onScreenOffset);
             },
           ),
           ScrollTransformItem(
-            builder: (offSet) {
-              return const OverlayTextSection();
-            },
-            offsetBuilder: (offset) => Offset(0, -screenSize.height),
+            builder: (scrollOffset) => const OverlayTextSection(),
+            offsetBuilder: (scrollOffset) => Offset(0, -screenSize.height),
           ),
-          ScrollTransformItem(builder: (offSet) => const BottomSection()),
+          ScrollTransformItem(builder: (scrollOffset) => const BottomSection()),
         ],
       ),
     );
